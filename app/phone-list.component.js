406f118680a9581139d567112d5d2f0538e4369d
VDNH <vdnh@yahoo.com>
2018-03-12 23:34:54 -0400

    Component

---
+++ app/phone-list.component.js
@@ -0,0 +1,32 @@
+'user strict';
+
+angular.
+	module('phonecatApp').
+	component('phoneList', {
+		template:
+			'<ul>' +
+    			'<li ng-repeat="phone in $ctrl.phones">' + 
+      			'<span>{{phone.name}}</span>' + 
+      			'<p>{{phone.snippet}}</p>' + 
+    			'</li>' + 
+    			'<p>Total of phones : {{phones.length}}  from the stock of {{$ctrl.name}}</p>' +
+  			'</ul>',
+  		controller: function PhoneListController(){
+  			this.phones = [
+  				{
+  					name : 'Nexus S',
+  					snippet : 'Fast just got faser with Nexus'
+  				},
+  				{
+  					name : 'Motorola S',
+  					snippet : 'The next generation'
+  				},
+  				{
+  					name : 'Dell TB',
+  					snippet : 'Tablet for the world.'
+  				}
+  			];
+  			this.name = 'vo dinh';
+  		}
+
+	});
